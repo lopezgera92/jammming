5e2872643ccd4bb62ca0eb989e450e0c70538f81
lopezgera92 <lopez.gerardoray@gmail.com>
2018-09-01 10:07:22 -0700

    add enter button functionality to search

--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -7,6 +7,7 @@ class SearchBar extends React.Component {
 
         this.search = this.search.bind(this);
         this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
 
         this.state = {term: ''};
     }
@@ -18,11 +19,17 @@ class SearchBar extends React.Component {
     handleTermChange(event) {
         this.setState({term: event.target.value});
     }
-    
+   
+    handleKeyPress(event) {
+        if(event.key == 'Enter') {
+            this.props.onSearch(this.state.term);
+        }
+    }
+
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onKeyPress={this.handleKeyPress}/>
                 <a onClick={this.search}>SEARCH</a>
             </div>
         )
